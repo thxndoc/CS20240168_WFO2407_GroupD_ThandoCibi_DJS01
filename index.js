d4e80d183e9cd8bc04dcaea76216016cba9061e3
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-10-22 15:29:55 +0200

    Edit varibale names to be more readable and semantic

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialVelocityAsKmPerHour = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
+const timeAsSeconds = 3600; // seconds (1 hour)
+const initialDistanceInKm = 0; // distance (km)
+const initialFuelInKg = 5000; // remaining fuel (kg)
+const fuelBurnRateInKg = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const updatedDistance = initialVelocityAsKmPerHour + (initialVelocityAsKmPerHour * timeAsSeconds) //calculates new distance
+const remainingFuel = fuelBurnRateInKg * timeAsSeconds //calculates remaining fuel
+const updatedVelocityAsKmPerHour = calcNewVel(initialVelocityInKmPerHour, acceleration, timeAsSeconds) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+calcNewVel = (initialVelocityAsKmPerHour, acceleration, timeAsSeconds) => { 
+  return initialVelocityAsKmPerHour + (acceleration * timeAsSeconds)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${updatedVelocityAsKmPerHour} km/h`);
+console.log(`Corrected New Distance: ${updateDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
